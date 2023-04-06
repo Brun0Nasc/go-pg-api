f01dbf90d5741abd13cc440a7729567d9f48d131
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2023-04-06 11:40:31 -0300

    feat: GetDiretorByID, ListDiretores e DeleteDiretor em diretor.controller.go

--- controllers/diretor.controller.go
+++ controllers/diretor.controller.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"net/http"
-
-	//"strconv"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	//"github.com/google/uuid"
+
 	db "github.com/Brun0Nasc/go-pg-api/db/sqlc"
 	"github.com/Brun0Nasc/go-pg-api/schemas"
 )
@@ -72,4 +71,77 @@ func (dc *DiretorController) UpdateDiretor(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"status": "success", "diretor": diretor})
+}
+
+func (dc *DiretorController) GetDiretorById(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	diretor, err := dc.db.GetDiretorById(ctx, uuid.MustParse(id))
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": "No director with taht ID exists"})
+			return
+		}
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "diretor": diretor})
+}
+
+func (dc *DiretorController) ListDiretores(ctx *gin.Context) {
+	var page = ctx.DefaultQuery("page", "1")
+	var limit = ctx.DefaultQuery("limit", "10")
+
+	intPage, err := strconv.Atoi(page)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+	intLimit, err := strconv.Atoi(limit)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	offset := (intPage - 1) * intLimit
+
+	args := &db.ListDiretoresParams{
+		Limit: int32(intLimit),
+		Offset: int32(offset),
+	}
+
+	diretores, err := dc.db.ListDiretores(ctx, *args)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	if diretores == nil {
+		diretores = []db.Diretor{}
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "results": len(diretores), "data": diretores})
+}
+
+func (dc *DiretorController) DeleteDiretor(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	_, err := dc.db.GetDiretorById(ctx, uuid.MustParse(id))
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": "No director with that ID exists"})
+			return
+		}
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	err = dc.db.DeleteDiretor(ctx, uuid.MustParse(id))
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success"})
 }
